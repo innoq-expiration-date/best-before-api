1b02b16571c531bf3c192ff2bd3a695ee1df3362
Dennis <dennis.stachowicz@innoq.com>
2022-06-17 14:10:41 +0200

    init with empty list

--- src/main/java/ch/vshn/hackvision/processing/DateExtractionResult.java
+++ src/main/java/ch/vshn/hackvision/processing/DateExtractionResult.java
@@ -1,10 +1,11 @@
 package ch.vshn.hackvision.processing;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DateExtractionResult {
     public String errorMessage;
     public boolean candidateFound;
     public String textContents;
-    public List<ExtractedDate> extractedDates;
+    public List<ExtractedDate> extractedDates = new ArrayList<>();
 }
